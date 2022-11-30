db64e96d644d263d83092d282f31e53de08467fd
wzsamuels <wzsamuels@gmail.com>
2022-11-29 22:22:04 -0500

    IDE and config files.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'a0.muscache.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
