6cbf47c57af553617142c7fcd192c6c5b6503430
wzsamuels <wzsamuels@gmail.com>
2023-03-27 23:46:18 -0400

    Complete checkout page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -4,9 +4,32 @@ import Image from "next/image";
 import Link from "next/link";
 import AccountMenu from "../components/AccountMenu";
 
-const Checkout = ({reservation, setReservation}) => {
-  const nights = reservation?.dates?.endDate && reservation?.dates?.startDate ? reservation.dates.endDate.diff(reservation.dates.startDate, 'day') : null;
+/// TODO: Add functionality to edit guests and dates from the checkout page
+
+const Checkout = ({reservation}) => {
   const [message, setMessage] = useState("");
+  const [insurance, setInsurance] = useState(false);
+
+  const nights = reservation?.dates?.endDate && reservation?.dates?.startDate ? reservation.dates.endDate.diff(reservation.dates.startDate, 'day') : null;
+
+  const calcTotal = () => {
+    let total = reservation?.listing?.price * nights ;
+    if(reservation?.listing?.cleaning_fee) {
+      total += reservation?.listing?.cleaning_fee;
+    }
+    if(reservation?.listing?.security_deposit) {
+      total += reservation?.listing?.security_deposit;
+    }
+    if(reservation?.guests?.adults + reservation?.guests?.children > reservation?.listing?.guests_included) {
+      total += reservation?.listing.extra_people;
+    }
+    if(insurance) {
+      total += 49.99;
+    }
+
+    return total;
+  }
+
   console.log(reservation)
 
   if(!reservation) {
@@ -35,14 +58,15 @@ const Checkout = ({reservation, setReservation}) => {
           <div className={'flex '}>
             <Image src={reservation?.listing?.images.picture_url} className={'h-auto mr-4 rounded-xl'} width={300} height={300} alt={"Listing Image"}/>
             <div>
-              <p>{reservation?.listing?.property_type}</p>
-              <p>{reservation?.listing?.bedrooms} bedrooms</p>
+              <p className={'font-bold'}>{reservation?.listing?.property_type}</p>
+              <p className={'my-2 font-bold'}>{reservation?.listing?.bedrooms} bedrooms</p>
+              <p>{reservation?.listing?.summary}</p>
             </div>
           </div>
 
           <hr className={'my-2'}/>
           <section className={'my-4'}>
-            <h2 className={'text-xl md:text-2xl my-2 font-bold'}>Trip Details</h2>
+            <h2 className={'text-xl md:text-2xl my-4'}>Trip Details</h2>
             <h3 className={'font-bold my-2'}>Dates</h3>
             <div>{reservation?.dates?.startDate.format('MMM D')} - {reservation?.dates?.endDate.format('MMM D')}</div>
 
@@ -53,38 +77,65 @@ const Checkout = ({reservation, setReservation}) => {
 
           <hr className={'my-2'}/>
           <section>
-            <h2 className={'text-xl md:text-2xl'}>Travel Insurance</h2>
-            <p>Protect against</p>
+            <h2 className={'text-xl md:text-2xl my-4'}>Travel Insurance</h2>
+            <p className={'my-4'}>Protect against the unexpected. Add peace of mind to your trip.</p>
+            <div className={'flex items-center'}>
+              <input type={'checkbox'} className={'rounded mr-2'} checked={insurance} onChange={() => setInsurance(state => !state)}/>
+              <label>Add Travel Insurance for $49.99</label>
+            </div>
           </section>
 
           <hr className={'my-2'}/>
-          <section>
-            <h2>Trip Price</h2>
-            <div className={'flex justify-between w-full'}>
-              <span>${reservation?.listing?.price} x {nights} nights</span>
+          <section className={''}>
+            <h2 className={'text-xl md:text-2xl my-4'}>Trip Price</h2>
+            <div className={'flex justify-between w-full my-2'}>
+              <span>${reservation?.listing?.price} per night x {nights} nights</span>
               <span>${reservation?.listing?.price * nights}</span>
             </div>
 
             { reservation?.listing?.cleaning_fee &&
-              <div className={'flex justify-between w-full mt-2'}>
+              <div className={'flex justify-between w-full my-2'}>
                 <span>Cleaning Fee</span>
                 <span>${reservation?.listing?.cleaning_fee}</span>
               </div>
             }
 
             { reservation?.listing?.security_deposit &&
-              <div className={'flex justify-between w-full mt-2'}>
+              <div className={'flex justify-between w-full my-2'}>
                 <span>Security Deposit</span>
                 <span>${reservation?.listing?.security_deposit}</span>
               </div>
             }
 
             { reservation?.guests?.adults + reservation?.guests?.children > reservation?.listing?.guests_included ?
-              <div className={'flex justify-between w-full mt-2'}>
+              <div className={'flex justify-between w-full my-2'}>
                 <span>Extra Guests</span>
                 <span>${reservation?.listing.extra_people}</span>
               </div>
               : null
+            }
+
+            { insurance &&
+              <div className={'flex justify-between w-full my-2'}>
+                <span>Travel Insurance</span>
+                <span>$49.99</span>
+              </div>
+            }
+            <div className={'flex justify-between w-full mt-2'}>
+              <span>Total: </span>
+              <span>${calcTotal()}</span>
+            </div>
+          </section>
+          <hr className={'my-2'}/>
+          <section className={'my-4 flex justify-center'}>
+            <button onClick={() => setMessage("You reservation has been confirmed. Enjoy your trip!")} className={'px-4 py-2 bg-red-600 rounded-xl text-white'}>Confirm Reservation</button>
+          </section>
+          <section className={'w-full flex justify-center'}>
+            {
+              message &&
+              <div className={'text-center rounded-xl border border-red-600 p-6 max-w-2xl w-full'}>
+                {message}
+              </div>
             }
           </section>
         </div>
